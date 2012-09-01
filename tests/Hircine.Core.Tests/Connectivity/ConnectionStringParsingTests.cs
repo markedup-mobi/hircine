1897a6206d612a5f4af3177a7b33027fd0587dd9
Aaronontheweb <aaron@aaronstannard.com>
2012-08-31 21:17:10 -0700

    Putting the connection string manager into test coverage mostly as a sanity check.

---
+++ tests/Hircine.Core.Tests/Connectivity/ConnectionStringParsingTests.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+
+namespace Hircine.Core.Tests.Connectivity
+{
+    [TestFixture(Description = "Test fixture which asserts that our RavenDB connection string parser behaves as expected for the defaults we show on the command-line")]
+    public class ConnectionStringParsingTests
+    {
+        #region Setup / Teardown
+
+
+
+        #endregion
+
+        #region Tests
+
+        [Test(Description = "Should accept a connection string that has just a Url in it")]
+        public void Should_Accept_ConnectionString_with_Just_Url()
+        {
+            //valid connection string
+            var connectionString = "http://localhost:8080";
+
+            var parsedConnectionString = 
+        }
+
+        #endregion
+    }
+}

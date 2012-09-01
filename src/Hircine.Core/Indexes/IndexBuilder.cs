149afba05de7651c714226ed4530bd434a5656a6
Aaron Stannard <aaron@aaronstannard.com>
2012-09-01 00:39:22 -0700

    Created the IndexBuilder

---
+++ src/Hircine.Core/Indexes/IndexBuilder.cs
@@ -0,0 +1,9 @@
+﻿namespace Hircine.Core.Indexes
+{
+    /// <summary>
+    /// Class responsible for managing an entire index building job against a database
+    /// </summary>
+    public class IndexBuilder
+    {
+    }
+}

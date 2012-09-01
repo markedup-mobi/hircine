0f730e3ce99f2d3f550f3dead112aaf2a610f9d2
Aaron Stannard <aaron@aaronstannard.com>
2012-08-31 23:51:17 -0700

    defined some test indexes and models

---
+++ src/Hircine.TestIndexes/Reducers/PostCountStats.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Hircine.TestIndexes.Reducers
+{
+    public class PostCountStats
+    {
+        public string Email { get; set; }
+        public int PostCount { get; set; }
+        public DateTimeOffset LastActivity { get; set; }
+    }
+}
---
+++ src/Hircine.TestIndexes/Reducers/WordCountStats.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Hircine.TestIndexes.Reducers
+{
+    public class WordCountStats
+    {
+        public string AuthorId { get; set; }
+        public string AuthorName { get; set; }
+        public int TotalPosts { get; set; }
+        public int TotalWordCount { get; set; }
+        public double AverageWordCountPerPost { get; set; }
+    }
+}

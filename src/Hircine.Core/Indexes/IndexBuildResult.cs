5f2da4bb8184b17f1b6e1c4190f3cd7c0d61b1d3
Aaron Stannard <aaron@aaronstannard.com>
2012-09-01 01:22:15 -0700

    created the first cut of the index builder

---
+++ src/Hircine.Core/Indexes/IndexBuildResult.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Hircine.Core.Indexes
+{
+    public class IndexBuildReport
+    {
+        public List<IndexBuildResult> BuildResults { get; set; }
+        public int Completed
+        {
+            get { return BuildResults.Count(x => x.Result == BuildResult.Success); }
+        }
+
+        public int Cancelled
+        {
+            get { return BuildResults.Count(x => x.Result == BuildResult.Cancelled); }
+        }
+
+        public int Failed
+        {
+            get { return BuildResults.Count(x => x.Result == BuildResult.Failed); }
+        }
+
+        public IndexBuildReport()
+        {
+            BuildResults = new List<IndexBuildResult>();
+        }
+    }
+
+    public class IndexBuildResult
+    {
+        public string IndexName { get; set; }
+        public BuildResult Result { get; set; }
+    }
+
+    public enum BuildResult
+    {
+        Success = 0,
+        Failed = 1,
+        Cancelled = 2
+    };
+}
--- src/Hircine.Core/Indexes/IndexBuilder.cs
+++ src/Hircine.Core/Indexes/IndexBuilder.cs
@@ -1,9 +1,86 @@
-﻿namespace Hircine.Core.Indexes
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Threading.Tasks;
+using Hircine.Core.Runtime;
+using Raven.Client;
+using Raven.Client.Indexes;
+
+namespace Hircine.Core.Indexes
 {
     /// <summary>
     /// Class responsible for managing an entire index building job against a database
     /// </summary>
-    public class IndexBuilder
+    public class IndexBuilder : IDisposable
     {
+        private readonly IDocumentStore _documentStore;
+        private readonly Assembly _indexAssembly;
+
+        public IndexBuilder(IDocumentStore store, Assembly indexAssembly)
+        {
+            _documentStore = store;
+            _indexAssembly = indexAssembly;
+        }
+
+        public Task<IndexBuildReport> Run(Action<IndexBuildResult> progressCallBack)
+        {
+            //Load our indexes
+            var indexes = AssemblyRuntimeLoader.GetRavenDbIndexes(_indexAssembly);
+            var tasks = new List<Task<IndexBuildResult>>();
+
+            foreach (var index in indexes)
+            {
+                var indexInstance = (AbstractIndexCreationTask)Activator.CreateInstance(index);
+
+                //Asynchronously work on executing the index
+                tasks.Add(indexInstance
+                    .ExecuteAsync(_documentStore.AsyncDatabaseCommands, _documentStore.Conventions)
+                    .ContinueWith(result =>
+                                      {
+                                          var indexBuildResult = new IndexBuildResult() { IndexName = indexInstance.IndexName };
+
+                                          if (result.IsCompleted)
+                                          {
+                                              indexBuildResult.Result = BuildResult.Success;
+                                          }
+                                          else if (result.IsCanceled)
+                                          {
+                                              indexBuildResult.Result = BuildResult.Cancelled;
+                                          }
+                                          else
+                                          {
+                                              indexBuildResult.Result = BuildResult.Failed;
+                                          }
+
+                                          if (progressCallBack != null)
+                                          {
+                                              progressCallBack.Invoke(indexBuildResult);
+                                          }
+
+                                          return indexBuildResult;
+                                      }));
+            }
+
+            return Task.Factory.ContinueWhenAll(tasks.ToArray(), cont =>
+                                                                     {
+                                                                         var buildReport = new IndexBuildReport()
+                                                                                               {
+                                                                                                   BuildResults =  tasks.Select(x => x.Result).ToList()
+                                                                                               };
+
+                                                                         return buildReport;
+                                                                     });
+        }
+
+        #region Implementation of IDisposable
+
+        public void Dispose()
+        {
+            if (_documentStore != null)
+                _documentStore.Dispose();
+        }
+
+        #endregion
     }
 }

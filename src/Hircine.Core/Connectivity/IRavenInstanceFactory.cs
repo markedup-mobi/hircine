56aad2502a6293564ba430f024b4f6e2d9c1c04b
Aaronontheweb <aaron@aaronstannard.com>
2012-08-31 21:10:38 -0700

    Set up some simple classes to create Raven instances and pass connection string instances along to them

---
+++ src/Hircine.Core/Connectivity/IRavenInstanceFactory.cs
@@ -0,0 +1,24 @@
+﻿using Raven.Client;
+
+namespace Hircine.Core.Connectivity
+{
+    /// <summary>
+    /// Interface used for our IDocumentStore / RavenDB instance Factory
+    /// </summary>
+    public interface IRavenInstanceFactory
+    {
+        /// <summary>
+        /// Get a RavenDB instance for a database we access over HTTP
+        /// </summary>
+        /// <param name="connectionString">The RavenDB connection string itself</param>
+        /// <returns>An UN-INITIALIZED (.Initialize() has not been called) RavenDB IDocumentStore for the provided connection string</returns>
+        IDocumentStore GetRavenConnection(string connectionString);
+
+        /// <summary>
+        /// Gets an Embedded RavenDB instance
+        /// </summary>
+        /// <param name="runInMemory">Flag that can be set to determine if this instance is in-memory only or not. Defaults to true.</param>
+        /// <returns>An UN-INITIALIZED (.Initialize() has not been called) Embedded RavenDB IDocumentStore</returns>
+        IDocumentStore GetEmbeddedInstance(bool runInMemory = true);
+    }
+}
---
+++ src/Hircine.Core/Connectivity/RavenConnectionStringParser.cs
@@ -0,0 +1,17 @@
+﻿using Raven.Abstractions.Data;
+
+namespace Hircine.Core.Connectivity
+{
+    /// <summary>
+    /// Static utility class designed to simplify the current awkwardness around parsing RavenDB connection strings.
+    /// </summary>
+    public static class RavenConnectionStringParser
+    {
+        public static RavenConnectionStringOptions ParseNetworkedDbOptions(string connectionString)
+        {
+            var connectionStringParser = ConnectionStringParser<RavenConnectionStringOptions>.FromConnectionString(connectionString);
+            connectionStringParser.Parse();
+            return connectionStringParser.ConnectionStringOptions;
+        }
+    }
+}

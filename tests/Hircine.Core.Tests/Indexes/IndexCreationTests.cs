bd4aaceaf47e460e4e7ca9e6472b270f57e29972
Aaron Stannard <aaron@aaronstannard.com>
2012-09-01 01:31:22 -0700

    Created test fixture to start integration testing our index-creation mechanism

---
+++ tests/Hircine.Core.Tests/Indexes/IndexCreationTests.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace Hircine.Core.Tests.Indexes
+{
+    [TestFixture(Description = "Test fixture used for verifying that we can successfully create indexes using IndexBuilder")]
+    public class IndexCreationTests
+    {
+    }
+}

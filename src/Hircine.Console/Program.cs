21e3915f14a2398ab6505114fa75330d2866c87c
Aaron Stannard <aaron@aaronstannard.com>
2012-09-01 17:27:25 -0700

    created our argument parser

--- src/Hircine.Console/Program.cs
+++ src/Hircine.Console/Program.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Hircine.Core;
+using Mono.Options;
 
 namespace Hircine.Console
 {
@@ -9,6 +11,33 @@ namespace Hircine.Console
     {
         static void Main(string[] args)
         {
+            var showHelp = false;
+            OptionSet p = null;
+            try
+            {
+                var indexCommand = IndexCommandBuilder.ParseIndexBuildCommand(args, out showHelp, out p);
+            }
+            catch (OptionException e)
+            {
+                System.Console.Write("hircine: ");
+                System.Console.WriteLine(e.Message);
+                System.Console.WriteLine("Try 'hircine --help' for more information.");
+            }
+
+            if (showHelp)
+            {
+                ShowHelp(p);
+            }
+        }
+
+        static void ShowHelp(OptionSet p)
+        {
+            System.Console.WriteLine("Usage: Hircine [OPTIONS]+");
+            System.Console.WriteLine("Greet a list of individuals with an optional message.");
+            System.Console.WriteLine("If no message is specified, a generic greeting is used.");
+            System.Console.WriteLine();
+            System.Console.WriteLine("Options:");
+            p.WriteOptionDescriptions(System.Console.Out);
         }
     }
 }
